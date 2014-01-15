cbf93bcac0a8d75c9860ca90be1106851d3bcc6c
taylorjames9 <taylorjames9@gmail.com>
2014-01-15 13:46:14 -0600

    InitializingAGitRepoForPowerSpots

---
+++ Assets/Scripts/GameDots_Manager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameDots_Manager : MonoBehaviour {
+
+	public GameObject dot;
+	public int numberOfSpots;
+	public float yMax = 5.35f;
+	public float yMin = -3.35f;
+	public float xMax = 8.19f;
+	public float xMin = -8.29f;
+	public float zMax = 2.0f;
+	public float zMin = -2.0f;
+	public List<GameObject> myListOfDots = new List<GameObject> ();
+	
+	// Use this for initialization
+	void Start () {
+		numberOfSpots = 10;
+		GenerateDots ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if New Dots button is hit...then call the generate dots method
+	
+	}
+
+	public void GenerateDots(){
+		for (int i = 0; i < numberOfSpots; i++) {
+			GameObject dizzyDot = (GameObject)Instantiate(dot);;
+			Vector3 dotPosition = new Vector3 (
+				                      Random.Range (xMin, xMax),
+				                      Random.Range (yMin, yMax),
+									  Random.Range (zMin, zMax));
+
+			dizzyDot.transform.localPosition = dotPosition;
+			dizzyDot.renderer.material.color = getRandomColor ();
+			myListOfDots.Add (dizzyDot);
+
+		foreach (GameObject listDot in myListOfDots) {
+
+		}
+
+		}
+		foreach (GameObject dippDot in myListOfDots){
+			Debug.Log(dippDot);
+		}
+	}
+
+	public Color32 getRandomColor () {
+		return new Color32 ((byte)Random.Range (0, 255), (byte)Random.Range (0, 255), (byte)Random.Range (0, 255), 1);  
+	}
+
+	public void ClearOldListODots (){
+		myListOfDots.Clear ();
+	}
+
+}
---
+++ Assets/Scripts/GuiScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GuiScript : MonoBehaviour {
+
+
+	//public GUIText new_dots;
+	GameDots_Manager gameDotManager;
+	public Vector2 MousePosition;
+
+
+	// Use this for initialization
+	void Start () {
+
+		gameDotManager = GameObject.Find ("Game_Dots_Manager").GetComponent<GameDots_Manager> ();
+		//new_dots.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		int fingerCount = 0;
+		MousePosition = Input.mousePosition;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled && guiTexture.HitTest (touch.position)) {
+				fingerCount++;
+			}
+		}
+		if (fingerCount > 0) {
+			gameDotManager.GenerateDots();
+			//Debug.Log ("Should have created a bunch of dots");
+			fingerCount = 0;
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			gameDotManager.ClearOldListODots ();
+			gameDotManager.GenerateDots();
+		}
+	}
+}
